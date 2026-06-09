f6cb99259d5b11e50dadfde1ce25301936c52ca7
Cédric Belin <cedx@outlook.com>
2026-06-09 22:03:00 +0200

    Update the "Lint" script [skip ci]

--- Scripts/Lint.ps1
+++ Scripts/Lint.ps1
@@ -1,5 +1,5 @@
 using module PSScriptAnalyzer
 
 "Performing the static analysis of source code..."
-$PSScriptRoot, "Sources", "Tests" | Invoke-ScriptAnalyzer -Recurse
+$PSScriptRoot, "Sources", "Tests" | Invoke-ScriptAnalyzer -ExcludeRule PSUseShouldProcessForStateChangingFunctions -Recurse
 Test-ModuleManifest Akismet.psd1 | Out-Null
